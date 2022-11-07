b05dcd7aa259390ae1c8fb3b36b0d910dc13eb7c
Bas Kooijman <newcityofatlantis@gmail.com>
2022-11-07 11:00:03 +0100

    added connection factory

--- src/Connection/MySql.php
+++ src/Connection/MySql.php
@@ -4,5 +4,58 @@ namespace Atlantisgroupbv\Model\Connection;
 
 class MySql
 {
+    /**
+     * @var string
+     */
+    private string $host;
 
+    /**
+     * @var string
+     */
+    private string $user;
+
+    /**
+     * @var string
+     */
+    private string $pass;
+
+    /**
+     * @var array
+     */
+    private array $dbname;
+
+    /**
+     * @var string
+     */
+    private string $port;
+
+    /**
+     * @var string
+     */
+    private string $charset;
+
+    /**
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
+
+    /**
+     * @param string $host
+     * @param string $user
+     * @param string $pass
+     * @param array $dbname
+     * @param string $port
+     * @param string $charset
+     * @param LoggerInterface $logger
+     */
+    public function __construct(string $host, string $user, string $pass, array $dbname, string $port, string $charset, LoggerInterface $logger)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->dbname = $dbname;
+        $this->port = $port;
+        $this->charset = $charset;
+        $this->logger = $logger;
+    }
 }
